810065406f918c90730d61e389771fad2478ed7a
yashar <yashar@DESKTOP-TQG6630>
2023-09-27 18:18:31 -0400

    Filler pages for start of lab

---
+++ src/Confirmation.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const confirmation = () => {    
+    let title = "Confirmation Page"
+    return (
+        <div>
+            <h1>
+                {title}
+            </h1>
+        </div>
+    )
+}
+
+export default confirmation
---
+++ src/purchase.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const purchase = () => {    
+    let title = "Purchase Page"
+    return (
+        <div>
+            <h1>
+                {title}
+            </h1>
+        </div>
+    )
+}
+
+export default purchase
