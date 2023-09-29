cb7521a9642f97cfd9a7c7119d650af556e44212
Mac <macray@DESKTOP-QUJ7R9E>
2023-09-29 17:38:01 -0400

    Changes to purchase.js and purchase.css. tmp changes

--- src/purchase.js
+++ src/purchase.js
@@ -1,14 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import "./components/purchase.css";
 
-const purchase = () => {    
+const Purchase = () => {    
+    const [order, setOrder] = useState({
+        productName: ["Apple", "Orange", "Banana", "Mango", "Kiwi"],
+        productDescription: ["Apple2", "Orange2", "Banana3", "Mango4", "Kiwi5"],
+        productPrice: [1, 2, 3, 4, 5],
+        buyQuantity: [0,0,0,0,0], 
+        creditCardNumber: '', 
+        expirDate: '', 
+        cvvCode: '',
+        cardHolderName: '', 
+        addressOne: '', 
+        adressTwo: '', 
+        city: '', 
+        state: '', 
+        zip:'',
+    });
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        navigate('/purchase/paymentEntry', {order: order, setOrder: setOrder});
+    }
+
+    console.log('order: ', order);
+
     let title = "Purchase Page"
     return (
         <div>
-            <h1>
-                {title}
-            </h1>
+            <form onSubmit={handleSubmit}>
+                {order.productName.map((productName, index) => (
+                    <div key={index} className="product-container">
+                        <div className="product-info">
+                            <label>{productName}</label>
+                            <p>Description: {order.productDescription[index]}</p>
+                            <p>Price: ${order.productPrice[index]}</p>
+                            <input
+                            type="number"
+                            required
+                            onChange={(e) => {
+                                const updatedOrder = { ...order };
+                                updatedOrder.buyQuantity[index] = e.target.value;
+                                setOrder(updatedOrder);
+                            }}
+                        />
+                        <br />
+                        </div>
+                    </div>
+                ))}   
+                <button className='button'>Pay</button>
+            </form>
         </div>
     )
 }
 
-export default purchase
+export default Purchase
