fa3d72f56e33992e28481c69b4816e5210091503
bakhshaeisarand.2@osu.edu <intactyashar@gmail.com>
2023-09-29 17:58:41 -0400

    fixed purchase.js runtime errors

---
+++ src/paymentEntry.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const paymentEntry = () => {
+    let title = "Shipping Entry Page"
+    return (
+        <div>
+            <h1>
+                {title}
+            </h1>
+        </div>
+    )
+}
+
+export default paymentEntry
