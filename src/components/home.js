93209cbce7facdfcc93af19ceea578cef37eefcc
HunterAshburn <85366454+HunterAshburn@users.noreply.github.com>
2023-10-03 23:34:05 -0400

    product images and cleaned up home page

--- src/components/home.js
+++ src/components/home.js
@@ -3,10 +3,11 @@ import React from "react";
 const Home = () => {
   
   return (
-    <div className="container">
-        <h1>CSE 5123 LLC.</h1>
-        <p>Your one-stop shop for tech</p>
-      <div className="row">
+    <div className="container" style={{ margin: "20px auto", padding: "20px" }}>
+      <h1 style={{fontWeight: "bold"}}>CSE 5123 LLC.</h1>
+      <h2>Your one-stop shop for tech</h2>
+      <hr style={{ marginBottom: "25px", marginTop: "25px" }}></hr>
+      <div className="row text-center">
         <div className="col-md-4">
           <h3>Mission and Vision</h3>
           <p>It is our mission to allow people to stay up to date by supplying them with the latest tech.</p>
