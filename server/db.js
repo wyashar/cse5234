664c1284f065922c3a0fb89ee6cabd0d8bc1555f
bakhshaeisarand.2@osu.edu <intactyashar@gmail.com>
2023-10-18 19:31:21 -0400

    sample database records and new post for quantity updating

--- server/db.js
+++ server/db.js
@@ -2,7 +2,6 @@ var mySql = require("sync-mysql")
 var connection = new mySql({
     host: "localhost",
     user: "root",
-    password: "password",
     database: "db"
 })
 module.exports = connection;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,69 @@ const app = express()
 const port = 7000
 const cors = require('cors')
 
+const drop_product = "DROP TABLE IF EXISTS Product"
+const create_product = (
+    "CREATE TABLE Product ( \
+        id int NOT NULL AUTO_INCREMENT, \
+        name varchar(255), \
+        quantity int, \
+        description text, \
+        price double, \
+        PRIMARY KEY (id) );"
+)
+
+const add_iPhone13 = (
+"INSERT INTO Product (name, quantity, description, price) \
+    VALUES \
+    ( \
+        'iPhone13', \
+        100, \
+        'The latest iPhone model with a powerful A15 Bionic chip and a stunning Super Retina XDR display', \
+        799 \
+    );"
+)
+
+const add_SamsungGalaxyWatch4 = (
+    "INSERT INTO Product (name, quantity, description, price) \
+    VALUES \
+    ( \
+        'Samsung Galaxy Watch 4', \
+        100, \
+        'A feature-packed smartwatch with health and fitness tracking, AMOLED display, and long battery life', \
+        249 \
+    );"
+)
+
+const add_SonyWH1000XM4Headphones =
+    "INSERT INTO Product (name, quantity, description, price) \
+    VALUES \
+    ( \
+        'Sony WH-1000XM4 Headphones', \
+        100, \
+        'Premium noise-canceling headphones with excellent sound quality and all-day comfort', \
+        349 \
+    );"
+
+const add_InstantPotDuoEvoPlus =
+"INSERT INTO Product (name, quantity, description, price) \
+VALUES \
+( \
+    'Instant Pot Duo Evo Plus', \
+    100, \
+    'A versatile multicooker that can pressure cook, sauté, steam, and more, making meal prep a breeze', \
+    349 \
+);"
+
+const add_NintendoSwitch =
+"INSERT INTO Product (name, quantity, description, price) \
+VALUES \
+( \
+    'Nintendo Switch', \
+    100, \
+    'A popular gaming console that offers both portable and TV modes for gaming on the go or at home', \
+    299 \
+);"
+
 const corsOptions = {
     origin: "http://localhost:3000",
     credentials: true,
@@ -13,6 +76,25 @@ app.use(express.json())
 app.listen(port, () => {
     console.log('RUNNING ON http://localhost:7000')
 })
-app.get("/", function(req, res){
-    res.send("HELLO!")
+app.get("/get_product", function(req, res){
+    db.query(drop_product)
+    db.query(create_product)
+    db.query(add_iPhone13)
+    db.query(add_SonyWH1000XM4Headphones)
+    db.query(add_SamsungGalaxyWatch4)
+    db.query(add_NintendoSwitch)
+    db.query(add_InstantPotDuoEvoPlus)
+    const result = db.query('select * from Product')
+    return res.send(result)
 })
+
+app.post("/update_quantity", function(req, res){
+    let IDs = req.body.names;
+    let quantity = req.body.quantity;
+    IDs.forEach((id_, index) => {
+        const currentQuantity = quantity[index];
+        const result = db.query(`UPDATE Product SET quantity = ${currentQuantity} WHERE id = ${id_};`);
+    });
+    return res.send('UPDATE PRODUCT QUANTITIES')
+});
+
