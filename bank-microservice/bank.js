1ef2fc02590d2fdb644fb0ca1bfe024b17378b09
yashar <yashar@DESKTOP-TQG6630>
2023-11-08 18:58:47 -0500

    scaffoling for sample bank microservice

---
+++ bank-microservice/bank.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+const port = 8000
+const cors = require('cors')
+
+const corsOptions = {
+    origin: "http://localhost:7000",
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.listen(port, () => {
+    console.log('RUNNING ON http://locatlhost:8000')
+})
+
+app.post('/can_pay', (req, res) => {
+    const info = req.body
+    const companyName = info.companyName
+    const companyNumber = info.companyNumber
+    const customerName = info.customerName
+    const customerCardNum = info.customerCardNum
+    const customerExpDate = info.customerExpDate
+    const customerCVV = info.customerCVV
+})
